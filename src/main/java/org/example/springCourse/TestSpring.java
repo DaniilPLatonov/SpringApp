90077b03742f4c543b9ef81d4bb0901ec644cd95
DaniilPlatonov <d_platonov@internet.ru>
2023-02-07 18:47:39 +0300

    Initial commit

--- src/main/java/org/example/springCourse/TestSpring.java
+++ src/main/java/org/example/springCourse/TestSpring.java
@@ -6,10 +6,27 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+
+        System.out.println(classicalMusic.getSong());
+        /*        MusicPlayer firstmusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondmusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        boolean compatison = firstmusicPlayer == secondmusicPlayer;
+
+        System.out.println(compatison);
+        System.out.println(firstmusicPlayer);
+        System.out.println(secondmusicPlayer);
+
+        firstmusicPlayer.setVolume(10);
+        System.out.println(firstmusicPlayer.getVolume());
+        System.out.println(secondmusicPlayer.getVolume());*/
+
+
+
+       // System.out.println(musicPlayer.getName());
+        // System.out.println(musicPlayer.getVolume());
         context.close();
     }
 }
